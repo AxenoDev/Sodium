c3aeb42697cb95d081bdb6d0db6fc3bf59268e2e
AxenoDev <tbavart@gmail.com>
2024-10-12 12:15:56 +0200

    👋Initial Commit

--- src/main/java/me/axeno/sodium/commands/AlertCommand.java
+++ src/main/java/me/axeno/sodium/commands/AlertCommand.java
@@ -2,6 +2,7 @@ package me.axeno.sodium.commands;
 
 import me.axeno.sodium.utils.constant.MessageManager;
 import me.axeno.sodium.utils.constant.MessageType;
+import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 import revxrsal.commands.annotation.Command;
 import revxrsal.commands.annotation.Named;
@@ -9,7 +10,7 @@ import revxrsal.commands.annotation.Named;
 public class AlertCommand
 {
     @Command("alert")
-    public void onAlertCommand(Player player, @Named("Alert Message") String[] message)
+    public void onAlertCommand(CommandSender sender, @Named("Alert Message") String[] message)
     {
         StringBuilder builder = new StringBuilder();
         for (String s : message)
@@ -17,7 +18,7 @@ public class AlertCommand
             builder.append(s).append(" ");
         }
         String alertMessage = builder.toString().trim();
-        for(Player online : player.getServer().getOnlinePlayers())
+        for(Player online : sender.getServer().getOnlinePlayers())
         {
             MessageManager.sendMessageType(online, alertMessage, MessageType.INFO, true);
         }
